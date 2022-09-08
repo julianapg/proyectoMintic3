c6f91ef115cd172e3832ff2348e4c2b8fa851920
= <=>
2022-09-08 06:46:08 -0500

    Post, patch, get, delete MovimientoDinero

--- project/src/main/java/com/mintic/project/controllers/ControladorMovimientoDinero.java
+++ project/src/main/java/com/mintic/project/controllers/ControladorMovimientoDinero.java
@@ -1,11 +1,11 @@
 package com.mintic.project.controllers;
 
+import com.mintic.project.models.Empresa;
+import com.mintic.project.models.MovimientoDinero;
 import com.mintic.project.repositories.RepositorioEmpresa;
 import com.mintic.project.repositories.RepositorioMovimientoDinero;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ControladorMovimientoDinero {
@@ -17,6 +17,34 @@ public class ControladorMovimientoDinero {
     @GetMapping("/movimientos")
     public String consultarMovimientos() {
         return repositorioMovimientoDinero.findAll().toString();
+    }
+
+    @CrossOrigin
+    @GetMapping("/movimientos/{id}")
+    public String consultarUnMovimiento(@PathVariable int id){
+        return repositorioMovimientoDinero.findById(id).toString();
+    }
+
+    @CrossOrigin
+    @DeleteMapping("/movimientos/{id}")
+    public String eliminarMovimiento(@PathVariable int id){
+        repositorioMovimientoDinero.deleteById(id);
+        return "Movimiento con id: "+id+" borrado";
+    }
+
+    @CrossOrigin
+    @PostMapping("/movimientos")
+    public String crearEmpresas(@RequestBody MovimientoDinero movimiento){
+        repositorioMovimientoDinero.save(movimiento);
+        return "Movimiento creado";
+    }
+
+    @CrossOrigin
+    @PatchMapping("/movimientos/{id}")
+    public String actualizarMovimiento(@PathVariable int id, @RequestBody MovimientoDinero movimiento){
+        movimiento.setId(id);
+        repositorioMovimientoDinero.save(movimiento);
+        return "Movimiento actualizado";
     }
 
 }
