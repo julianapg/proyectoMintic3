4b1c011d62550483a68acb7e91019e7d8b61e81a
julianapg <julianapg92@gmail.com>
2022-09-09 19:37:32 -0500

    Se creo el servicio movimientodinero

--- project/src/main/java/com/mintic/project/services/ServicioMovimientoDinero.java
+++ project/src/main/java/com/mintic/project/services/ServicioMovimientoDinero.java
@@ -1,5 +1,7 @@
 package com.mintic.project.services;
 
+import com.mintic.project.models.Empresa;
+import com.mintic.project.models.MovimientoDinero;
 import com.mintic.project.repositories.RepositorioMovimientoDinero;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,5 +13,27 @@ public class ServicioMovimientoDinero {
 
     public String consultarTodosPorIdEmpresa(int id){
         return repositorioMovimientoDinero.findByEmpresa_Id(id).toString();
+    }
+    public String consultarUno(int id){
+        return repositorioMovimientoDinero.findById(id).toString();
+    }
+
+    public void eliminar(int id){
+        repositorioMovimientoDinero.deleteById(id);
+    }
+
+    public void crear(int id, MovimientoDinero movimiento){
+        Empresa e1 = new Empresa();
+        e1.setId(id);
+        movimiento.setEmpresa(e1);
+        repositorioMovimientoDinero.save(movimiento);
+    }
+
+    public void actualizar(int id, int id2, MovimientoDinero movimiento){
+        movimiento.setId(id2);
+        Empresa e1 = new Empresa();
+        e1.setId(id);
+        movimiento.setEmpresa(e1);
+        repositorioMovimientoDinero.save(movimiento);
     }
 }
